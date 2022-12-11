423d8072fa65d51b06bd46a561229be8969dc3fb
Manasapothakamuru <115751923+Manasapothakamuru@users.noreply.github.com>
2022-12-10 21:25:45 -0800

    Update ALS_Final.py

--- ALS_Final.py
+++ ALS_Final.py
@@ -1,23 +1,12 @@
 import numpy as np
 import pandas as pd
 import cv2
-#from matplotlib import pyplot as plt
-#from _future_ import print_function
 import logging
-#import io
-#import sys
-#import os
-#Import svm model
-#from sklearn import svm
-#import csv
 import findspark
 findspark.init()
 findspark.find()
 import pyspark
 findspark.find()
-#Import scikit-learn metrics module for accuracy calculation
-#from sklearn import metrics
-#import pandas as pd
 import numpy as np
 import cv2
 import glob
@@ -42,40 +31,46 @@ from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.sql.types import StructType,StructField,IntegerType
 
-
-#conf= pyspark.SparkConf().setAppName('SparkApp').setMaster('local')
-#sc = SparkContext.getOrCreate()
+#create a spark session
 spark=SparkSession \
   .builder \
   .appName("Example") \
-  .config("spark.driver.memory", "16g")\
+  .config("spark.driver.memory", "2g")\
   .getOrCreate()
 
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 
-lines = spark.read.csv("C:\\Users\Manasapothakamuru\OneDrive - Cal State Fullerton\Desktop\ADBMS project\Main DataSet\Books_rating.csv")
-drop_column_list = ["_c1","_c2","_c4","_c5","_c7","_c8","_c9"]
+#read the data file books_ratings into the dataframe
+lines = spark.read.csv("C:\Users\Manasapothakamuru\OneDrive - Cal State Fullerton\Desktop\ADBMS project\Main DataSet\Books_rating.csv")
+
+
+#selecting the columns that are not required for the process
+drop_column_list = ["_c0","_c2","_c4","_c5","_c7","_c8","_c9"]
+
+#create a dataframe with only required columns and column names are user, item(book), ratings for the book
 df2 = lines.withColumnRenamed("_c3","user") \
-    .withColumnRenamed("_c0","item") \
+    .withColumnRenamed("_c1","item") \
     .withColumnRenamed("_c6","rating")
 
-
 df3 = df2.select([column for column in df2.columns if column not in drop_column_list]) 
-df3.printSchema()
-df3.show()
+
+#create indexers for the string columns, this will convert into indexers for the string datatypes 
 indexer = [StringIndexer(inputCol=column, outputCol=column+"_index") for column in ['user', 'item','rating']]
 for index in indexer:
     index.setHandleInvalid("skip").fit(df3).transform(df3).show
+
+# pipeline is created to input into the machine learning model
 pipeline = Pipeline(stages=indexer)
 transformed = pipeline.fit(df3).transform(df3)
 transformed.select(['user', 'item','rating','user_index', 'item_index','rating_index'])
 
-
+#data is split into training and test dataframes with 80% in training set and 20% in test set
 (training, test) = transformed.randomSplit([0.8, 0.2])
 
-
+#Alternating least squares is the algorithm that is used for the classification. 
+#indexed columns are sent as parameters to the model 
 als=ALS(maxIter=5,
         regParam=0.09,
         rank=25,
@@ -85,13 +80,19 @@ als=ALS(maxIter=5,
         coldStartStrategy="drop",
         nonnegative=True)
 
+#model is being trained on the training dataset
 model=als.fit(training)
+
+#evaluates the test dataset
 evaluator=RegressionEvaluator(metricName="rmse",labelCol="rating_index",predictionCol="prediction")
 predictions=model.transform(test)
 rmse=evaluator.evaluate(predictions)
-print("RMSE="+str(rmse))
+
+#root mean square error is found for the predicted test dataset
+#print("RMSE="+str(rmse))
 print("Accuracy="+str(1.96*rmse))
-test = model.recommendForAllUsers(20).filter(col('user_index')==30).select("recommendations").collect()
+userid=input("enter the user  id:")
+test = model.recommendForAllUsers(20 ).filter(col('user')==userid).select("recommendations").collect()
 books = []
 for item in test[0][0]:        
     books.append(item.item_index)
@@ -99,15 +100,10 @@ for item in test[0][0]:
 schema = StructType([StructField("item_index",IntegerType(),True)])
 books = spark.createDataFrame(books,IntegerType()).toDF("item_index")
 
+# recommends books based on the user id that is entered
 
 transformed\
 .select(['item', 'user', 'rating'])\
-.filter(col('user_index')==30)\
+.filter(col('user')==userid)\
+.drop_duplicates(subset=['item'])\
 .show()
-
-books\
-.join(transformed, on = 'item_index', how = 'inner')\
-.select(['item', 'rating', 'user'])\
-.drop_duplicates(subset=['user'])\
-.show()
-
